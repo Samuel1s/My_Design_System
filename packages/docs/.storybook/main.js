60cb7f0ef2949c4cfca7e10067410c1df47dd1b8
Samuel1s <samuelfds286@gmail.com>
2023-12-12 11:29:36 -0300

    wip: attempt 4, update the version and change the main file

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -1,47 +1,15 @@
-// import { join, dirname } from "path";
+import { join, dirname } from "path";
 
-// /**
-//  * This function is used to resolve the absolute path of a package.
-//  * It is needed in projects that use Yarn PnP or are set up within a monorepo.
-//  */
-// function getAbsolutePath(value) {
-//   return dirname(require.resolve(join(value, "package.json")));
-// }
+/**
+ * This function is used to resolve the absolute path of a package.
+ * It is needed in projects that use Yarn PnP or are set up within a monorepo.
+ */
+function getAbsolutePath(value) {
+  return dirname(require.resolve(join(value, "package.json")));
+}
 
-// const config = {
-//   stories: ["../src/pages/**/*.mdx", "../src/stories/**/*.stories.tsx"],
-//   addons: [
-//     getAbsolutePath("@storybook/addon-links"),
-//     getAbsolutePath("@storybook/addon-essentials"),
-//     getAbsolutePath("@storybook/addon-onboarding"),
-//     getAbsolutePath("@storybook/addon-interactions"),
-//     getAbsolutePath("@storybook/addon-a11y"),
-//   ],
-//   framework: {
-//     name: getAbsolutePath("@storybook/react-vite"),
-//     options: {},
-//   },
-//   core: {
-//     builder: getAbsolutePath("@storybook/builder-vite"),
-//   },
-//   features: {
-//     storyStoreV7: true,
-//   },
-//   viteFinal: (config, { configType }) => {
-//     if (configType === "PRODUCTION") {
-//       config.base = "/My_Design_System/";
-//     }
-
-//     if (configType === "DEVELOPMENT") {
-//       config.base = "/My_Design_System/";
-//     }
-
-//     return config;
-//   },
-// };
-// export default config;
-module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+const config = {
+  stories: ["../src/pages/**/*.mdx", "../src/stories/**/*.stories.tsx"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
@@ -49,19 +17,26 @@ module.exports = {
     "@storybook/addon-interactions",
     "@storybook/addon-a11y",
   ],
-  framework: "@storybook/react",
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
   core: {
     builder: "@storybook/builder-vite",
   },
   features: {
     storyStoreV7: true,
   },
-
   viteFinal: (config, { configType }) => {
     if (configType === "PRODUCTION") {
+      config.base = "/My_Design_System/";
+    }
+
+    if (configType === "DEVELOPMENT") {
       config.base = "/My_Design_System/";
     }
 
     return config;
   },
 };
+export default config;
