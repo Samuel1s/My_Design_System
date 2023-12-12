49d0b23822785ed26cfe46088b30d6cf9073cf38
Samuel1s <samuelfds286@gmail.com>
2023-12-12 11:48:49 -0300

    wip: attempt 5, remove viteFinal in mainJs file

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -11,32 +11,32 @@ function getAbsolutePath(value) {
 const config = {
   stories: ["../src/pages/**/*.mdx", "../src/stories/**/*.stories.tsx"],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-onboarding",
-    "@storybook/addon-interactions",
-    "@storybook/addon-a11y",
+    getAbsolutePath("@storybook/addon-links"),
+    getAbsolutePath("@storybook/addon-essentials"),
+    getAbsolutePath("@storybook/addon-onboarding"),
+    getAbsolutePath("@storybook/addon-interactions"),
+    getAbsolutePath("@storybook/addon-a11y"),
   ],
   framework: {
-    name: "@storybook/react-vite",
+    name: getAbsolutePath("@storybook/react-vite"),
     options: {},
   },
   core: {
-    builder: "@storybook/builder-vite",
+    builder: getAbsolutePath("@storybook/builder-vite"),
   },
   features: {
     storyStoreV7: true,
   },
-  viteFinal: (config, { configType }) => {
-    if (configType === "PRODUCTION") {
-      config.base = "/My_Design_System/";
-    }
+  // viteFinal: (config, { configType }) => {
+  //   if (configType === "PRODUCTION") {
+  //     config.base = "/My_Design_System/";
+  //   }
 
-    if (configType === "DEVELOPMENT") {
-      config.base = "/My_Design_System/";
-    }
+  //   if (configType === "DEVELOPMENT") {
+  //     config.base = "/My_Design_System/";
+  //   }
 
-    return config;
-  },
+  //   return config;
+  // },
 };
 export default config;
