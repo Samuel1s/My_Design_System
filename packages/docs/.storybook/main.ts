94264078ec9ba65ce482d3fd1c7f11e27a30782d
Samuel1s <samuelfds286@gmail.com>
2023-12-12 16:14:37 -0300

    fix: refresh project docs

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -1,21 +1,21 @@
+import type { StorybookConfig } from "@storybook/react-vite";
+
 import { join, dirname } from "path";
 
 /**
  * This function is used to resolve the absolute path of a package.
  * It is needed in projects that use Yarn PnP or are set up within a monorepo.
  */
-function getAbsolutePath(value) {
+function getAbsolutePath(value: string): any {
   return dirname(require.resolve(join(value, "package.json")));
 }
-
-const config = {
+const config: StorybookConfig = {
   stories: ["../src/pages/**/*.mdx", "../src/stories/**/*.stories.tsx"],
   addons: [
     getAbsolutePath("@storybook/addon-links"),
     getAbsolutePath("@storybook/addon-essentials"),
     getAbsolutePath("@storybook/addon-onboarding"),
     getAbsolutePath("@storybook/addon-interactions"),
-    getAbsolutePath("@storybook/addon-a11y"),
   ],
   framework: {
     name: getAbsolutePath("@storybook/react-vite"),
@@ -23,12 +23,6 @@ const config = {
   },
   docs: {
     autodocs: true,
-  },
-  core: {
-    builder: getAbsolutePath("@storybook/builder-vite"),
-  },
-  features: {
-    storyStoreV7: true,
   },
   viteFinal: (config, { configType }) => {
     if (configType === "PRODUCTION") {
--- packages/docs/.storybook/preview.ts
+++ packages/docs/.storybook/preview.ts
@@ -1,6 +1,6 @@
-/** @type { import('@storybook/react').Preview } */
+import type { Preview } from "@storybook/react";
 
-const preview = {
+const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
